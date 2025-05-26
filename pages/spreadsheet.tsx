5a2b589b0a679a0c4ced959519ddcbd8b4f5f78c
marceloadaes <81936661+marceloadaes@users.noreply.github.com>
2025-05-26 10:05:58 -0300

    Add navigable menu home page

---
+++ pages/spreadsheet.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import Layout from '../components/Layout';
+
+const Spreadsheet: NextPage = () => (
+  <Layout>
+    <h2>Spreadsheet</h2>
+    <p>This page will display your Google Sheet data.</p>
+  </Layout>
+);
+
+export default Spreadsheet;
