1f000e3bb206cbdc07fdea7321d651caaf43e6d6
marceloadaes <81936661+marceloadaes@users.noreply.github.com>
2025-05-26 22:36:04 -0300

    Show full user info on settings page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -15,6 +15,8 @@ const Settings: NextPage = () => {
   const [userEmail, setUserEmail] = useState('');
   const [userIcon, setUserIcon] = useState('');
   const [userName, setUserName] = useState('');
+  // Store full response from /api/userinfo for debugging
+  const [userInfo, setUserInfo] = useState<Record<string, any> | null>(null);
 
   useEffect(() => {
     fetch('/api/config')
@@ -33,16 +35,19 @@ const Settings: NextPage = () => {
           setUserEmail(data.email);
           setUserIcon(data.picture);
           setUserName(data.name);
+          setUserInfo(data);
         })
         .catch(() => {
           setUserEmail('');
           setUserIcon('');
           setUserName('');
+          setUserInfo(null);
         });
     } else {
       setUserEmail('');
       setUserIcon('');
       setUserName('');
+      setUserInfo(null);
     }
   }, [connected]);
 
@@ -179,6 +184,9 @@ const Settings: NextPage = () => {
           )}
         </div>
       </div>
+      {userInfo && (
+        <pre>{JSON.stringify(userInfo, null, 2)}</pre>
+      )}
     </Layout>
   );
 };
